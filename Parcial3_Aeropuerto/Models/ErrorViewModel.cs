df52f63b671d3ed67cd842485dc23e9342b083ca
Angel00333 <oh232100333@uma.edu.sv>
2026-03-21 11:50:33 -0600

    Agregar archivos de proyecto.

---
+++ Parcial3_Aeropuerto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial3_Aeropuerto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
