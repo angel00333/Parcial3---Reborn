94c031c8029f57c3a6f841359a13bd9e35eb5be3
Geovani-Moran <nilsongeovanilue@gmail.com>
2026-03-31 19:03:36 -0600

    Aerolineas-Funcional-Completo

---
+++ Parcial3_Aeropuerto/Controllers/AerolineasController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Parcial3_Aeropuerto.Models;
+using System.Data;
+using MySqlConnector;
+
+namespace Parcial3_Aeropuerto.Controllers
+{
+    public class AerolineasController : Controller
+    {
+        // GET: AerolieasController
+        public ActionResult Aerolineas(string buscar)
+        {
+            if (string.IsNullOrWhiteSpace(buscar))
+            {
+                return View(AerolineasDAL.MostrarAerolineas());
+            }
+
+            var resultado = AerolineasDAL.BuscarAerolineas(buscar);
+            return View(resultado);
+
+        }
+
+        // GET: AerolieasController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AerolieasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AerolieasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Aerolineas aerolineas)
+        {
+            ModelState.Remove("Id_aerolinea");
+            if (ModelState.IsValid)
+            {
+                AerolineasDAL.InsertarAerolineas(aerolineas);
+                return RedirectToAction("Aerolineas");
+            }
+
+            return View();
+          
+        }
+
+        // GET: AerolieasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var aerolineas = AerolineasDAL.ObtenerAerolineasPorId(id);
+            return View(aerolineas);
+        }
+
+        // POST: AerolieasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Aerolineas aerolineas)
+        {
+            if (ModelState.IsValid) {
+                AerolineasDAL.ModificarAerolineas(aerolineas);
+                return RedirectToAction("Aerolineas");
+
+                            
+            }
+            return View();
+
+        }
+
+        // GET: AerolieasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(AerolineasDAL.ObtenerAerolineasPorId(id));
+        }
+
+        // POST: AerolieasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Aerolineas aerolineas)
+        {
+          AerolineasDAL.EliminarAerolineas(aerolineas.Id_aerolinea);
+            return RedirectToAction("Aerolineas");
+        }
+    }
+}
---
+++ Parcial3_Aeropuerto/Models/AerolineasDAL.cs
@@ -0,0 +1,132 @@
+﻿
+using System.Data;
+using MySqlConnector;
+
+namespace Parcial3_Aeropuerto.Models
+{
+    public class AerolineasDAL
+    {
+        public static List<Aerolineas> MostrarAerolineas()
+        {
+            List<Aerolineas> aerolineaslist = new List<Aerolineas>();
+            using (MySqlConnection con = ConexionSQL.Conectar())
+            {
+                con.Open();
+                string sql = "SELECT * FROM Aerolineas";
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.CommandType = CommandType.Text;
+                IDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    Aerolineas aerolineas = new Aerolineas();
+                    aerolineas.Id_aerolinea = reader.GetInt32(0);
+                    aerolineas.Nombre_aerolinea = reader.GetString(1);
+                    aerolineaslist.Add(aerolineas);
+                }
+                con.Close();
+            }
+            return aerolineaslist;
+        }
+
+        public static List<Aerolineas> BuscarAerolineas(string criterio)
+        {
+            List<Aerolineas> aerolineaslist = new List<Aerolineas>();
+            using (MySqlConnection con = ConexionSQL.Conectar())
+            {
+                con.Open();
+                string sql = "SELECT * FROM Aerolineas WHERE Id_aerolinea LIKE @C OR Nombre_aerolinea LIKE @C";
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.Parameters.AddWithValue("@C", "%" + criterio + "%");
+                IDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    aerolineaslist.Add(new Aerolineas
+                    {
+                        Id_aerolinea = reader.GetInt32(0),
+                        Nombre_aerolinea = reader.GetString(1)
+                    });
+                }
+                con.Close();
+            }
+            return aerolineaslist;
+
+        }
+
+
+        public static int InsertarAerolineas(Aerolineas aerolineas)
+        {
+            int resultado = 0;
+            using (MySqlConnection con = ConexionSQL.Conectar())
+            {
+                con.Open();
+                string sql = "INSERT INTO Aerolineas (Nombre_aerolinea) VALUES (@Nombre_aerolinea)";
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.Parameters.AddWithValue("@Nombre_aerolinea", aerolineas.Nombre_aerolinea);
+                resultado = comando.ExecuteNonQuery();
+                con.Close();
+            }
+            return resultado;
+        }
+
+        public static int EliminarAerolineas(int id)
+        {
+            int resultado = 0;
+            using (MySqlConnection con = ConexionSQL.Conectar())
+            {
+                con.Open();
+                string sql = "DELETE FROM Aerolineas WHERE Id_aerolinea=@Id_aerolinea";
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.Parameters.AddWithValue("@Id_aerolinea", id);
+                resultado = comando.ExecuteNonQuery();
+                con.Close();
+            }
+            return resultado;
+
+
+
+        }
+
+        public static int ModificarAerolineas(Aerolineas aerolineas)
+        {
+            int resultado = 0;
+            using (MySqlConnection con = ConexionSQL.Conectar())
+            {
+                con.Open();
+                string sql = "UPDATE Aerolineas SET Nombre_aerolinea=@Nombre_aerolinea WHERE Id_aerolinea=@Id_aerolinea";
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.Parameters.AddWithValue("@Nombre_aerolinea", aerolineas.Nombre_aerolinea);
+                comando.Parameters.AddWithValue("@Id_aerolinea", aerolineas.Id_aerolinea);
+                resultado = comando.ExecuteNonQuery();
+                con.Close();
+            }
+            return resultado;
+        }
+
+        public static Aerolineas ObtenerAerolineasPorId(int id)
+        {
+            Aerolineas aerolineas = new Aerolineas();
+            using (MySqlConnection con = ConexionSQL.Conectar())
+            {
+                con.Open();
+                string sql = "SELECT * FROM Aerolineas WHERE Id_aerolinea=@Id_aerolinea";
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.Parameters.AddWithValue("@Id_aerolinea", id);
+                IDataReader reader = comando.ExecuteReader();
+                if (reader.Read())
+                {
+                    aerolineas = new Aerolineas
+                    {
+                        Id_aerolinea = reader.GetInt32(0),
+                        Nombre_aerolinea = reader.GetString(1)
+                    };
+                }
+                con.Close();
+            }
+            return aerolineas;
+
+
+
+
+        }
+    }
+}
