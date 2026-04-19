eaf0d410a9a17297f7b32e2a5a7a44df571848bb
Geovani-Moran <nilsongeovanilue@gmail.com>
2026-04-19 14:42:04 -0600

    0.3.9.1 - Aviones/Pasajero

--- Parcial3_Aeropuerto.EN/Aviones.cs
+++ Parcial3_Aeropuerto.EN/Aviones.cs
@@ -9,22 +9,20 @@ namespace Parcial3_Aeropuerto.EN
     {
         public int Id_avion { get; set; }
 
-        [Required(ErrorMessage = "El campo I_aerolineas es obligatorio")]
-        public Aerolineas Aerolineas { get; set; }
-
         [Required(ErrorMessage = "El campo Capacidad es obligatorio")]
         public int Capacidad { get; set; }
 
+        public int Id_aerolinea { get; set; } //agradado
         public string Nombre_aerolinea { get; set; } = string.Empty;
         //SE hace el mmado del nombre de la aerolinea
 
         public Aviones () { }
 
-        public Aviones(int id_avion, Aerolineas pAerolineas , int capacidad, string nombre_aerolinea)
+        public Aviones(int id_avion, Aerolineas pAerolineas , int capacidad, int id_aerolinea, string nombre_aerolinea)
         {
             Id_avion = id_avion;
-            Aerolineas = pAerolineas;
             Capacidad = capacidad;
+            Id_aerolinea = id_aerolinea;
             Nombre_aerolinea = nombre_aerolinea;
         }
     }
